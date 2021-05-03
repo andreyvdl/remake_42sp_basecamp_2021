78b59c923c9078f278806cfd0a7fa31fb5db1a83
andreyvdl <andreyvictordantasleal2016@gmail.com>
2021-05-03 17:13:33 -0300

    ex07 feito

---
+++ C00/ex07/ft_putnbr.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: avictor- <avictor-@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/03 16:23:56 by avictor-          #+#    #+#             */
+/*   Updated: 2021/05/03 16:23:56 by avictor-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_write(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	unsigned int nbr;
+
+	if (nb < 0)
+	{
+		ft_write('-');
+		nbr = nb * -1;
+	}
+	else
+		nbr = nb;
+	if (nbr >= 10)
+	{
+		ft_putnbr(nbr / 10);
+	}
+	ft_write(nbr % 10 + '0');
+}
