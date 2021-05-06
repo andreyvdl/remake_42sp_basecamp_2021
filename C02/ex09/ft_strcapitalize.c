ff45ace2e521c8150c6372c11444e1713ded7999
andreyvdl <andreyvictordantasleal2016@gmail.com>
2021-05-06 11:24:48 -0300

    fixing error

--- C02/ex09/ft_strcapitalize.c
+++ C02/ex09/ft_strcapitalize.c
@@ -22,9 +22,9 @@ char	*ft_strcapitalize(char *str)
 		if ((str[place] >= 'A' && str[place] <= 'Z') &&
 		((str[place - 1] >= 'A' && str[place - 1] <= 'Z') ||
 		(str[place - 1] >= 'a' && str[place - 1] <= 'z') ||
-		(str[place - 1] >= '0' && str[place - 1 <= '9'])))
+		(str[place - 1] >= '0' && str[place - 1] <= '9')))
 			str[place] += 32;
-		if ((str[place] >= 'a' && str[place] <= 'z') &&
+		else if ((str[place] >= 'a' && str[place] <= 'z') &&
 		(!((str[place - 1] >= 'A' && str[place - 1] <= 'Z') ||
 		(str[place - 1] >= 'a' && str[place - 1] <= 'z') ||
 		(str[place - 1] >= '0' && str[place - 1] <= '9'))))
